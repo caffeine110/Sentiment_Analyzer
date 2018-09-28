8a86fab8c31a5e4a4e25b0c2daa06d98d5309398
caffeine110 <gaurav.developer.audio@gmail.com>
2018-09-28 23:59:46 +0530

    updates

--- models/adaboost/adb_classifier.py
+++ models/adaboost/adb_classifier.py
@@ -69,25 +69,22 @@ print("no. of Sara training emails: {} ".format(len(Y_train)-sum(Y_train)))
 
 
 
+#import Classifier
+from sklearn.ensemble import AdaBoostClassifier
 
-#import Knn classifier
-from sklearn.neighbors import KNeighborsClassifier
-
-#initialising classifier1234
-knn_classifier = KNeighborsClassifier()
-
-
+#initialising classifier
+ada_classifier = AdaBoostClassifier(n_estimators=50, learning_rate=1, random_state=0)
 
 #fitting data to the classifer
-knn_classifier.fit(X_train, Y_train)
+ada_classifier.fit(features_train, labels_train)
 
 
 
 from sklearn.metrics import accuracy_score
 #prediction
-author_pred = knn_classifier.predict(X_test)
+author_pred = ada_classifier.predict(features_test)
 
 
 #printing the accuracy
-print(accuracy_score(X_test, author_pred))
-print(knn_classifier.score(X_test, Y_test))
+print(accuracy_score(labels_test, author_pred))
+print(ada_classifier.score(features_test, labels_test))
